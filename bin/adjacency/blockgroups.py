4ef8e90813a8ac1956d549239b0d65bbb42c8ac0
Remi Louf <remi@sciti.es>
2016-06-23 10:32:51 +0200

    Fix directory structure in blockgroup adjacency

--- bin/adjacency/blockgroups.py
+++ bin/adjacency/blockgroups.py
@@ -2,6 +2,7 @@
 
 Script to compute the adjacency list for the blockgroups in all CBSA.
 """
+import os
 import csv
 import sys
 import itertools
@@ -12,15 +13,19 @@ from shapely.geometry import shape
 csv.field_size_limit(sys.maxsize)
 
 
+
 #
-# Import list of cities 
+# Read preliminary data
 #
+
+# Names of CBSAs
 cbsa = {}
-with open('data/names/cbsa_names.txt', 'r') as source:
+with open('data/misc/cbsa_names.txt', 'r') as source:
     reader = csv.reader(source, delimiter='\t')
     reader.next()
     for rows in reader:
         cbsa[rows[0]] = rows[1]
+
 
 
 
@@ -43,8 +48,16 @@ for i,city in enumerate(cbsa):
         if blocks[b1].touches(blocks[b0]):
             adjacency[b0].append(b1)
 
-    ## Save data
-    with open('data/adjacency/cbsa/%s/%s_adjacency_blockgroups.txt'%(city,city), 'w') as output:
+
+    # Data export
+
+    ## Create directory
+    export_path = 'data/adjacency/cbsa/%s/'%(city)
+    if not os.path.exists(export_path):
+        os.makedirs(export_path)
+
+    ## Write data
+    with open(export_path + '%s_adjacency_blockgroups.txt'%city, 'w') as output:
        output.write("BLOCKGROUP FIPS\tNEIGHBOURS FIPS\n")
        for b0 in adjacency:
            output.write("%s"%b0)
